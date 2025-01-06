fdced8971dac5890126ba660afd291040ba667e5
Micheal Wong <wongkaishen2003@hotmail.com>
2025-01-06 20:52:43 +0800

    main

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -10,10 +10,7 @@ urlpatterns = [
     path('signin',views.signin,name="signin"),#redirect home after signin or signup
     path('signout',views.signout,name="signout"),#redirect home after signout
     path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'), #redirect to home after verification
-
     path('form',views.location_view,name="res.form"), #redirect to form
-
-
     path('restaurant/',views.get_res_list,name="res.res"), #for the restaurant list
     path('restaurant/<int:id>/', views.get_res_detail, name='restaurant_detail'), #for the restaurant detail id
     path('map/',views.get_res_map,name="res.map"),   #for the whole map view of the restaurant
