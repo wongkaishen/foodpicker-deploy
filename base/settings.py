8bec774eb3880e1413951737270538fe2d227488
Micheal Wong <wongkaishen2003@hotmail.com>
2025-04-15 20:39:25 +0800

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-
+    
 ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = "/signup/"
